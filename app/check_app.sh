730826238627e7c03d5fd74feea73b314998d929
user <laurent.fauveau@laplateforme.io>
2025-08-01 13:18:00 +0200

    Ajout dossier app avec index.html et script de vérification

---
+++ app/check_app.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Début de la vérification de l'application..."
+
+if [ -f "index.html" ]; then
+  echo "Fichier index.html trouvé. Vérification réussie."
+  echo "Fin de la vérification."
+  exit 0
+else
+  echo "Erreur : le fichier index.html est manquant."
+  echo "Fin de la vérification."
+  exit 1
+fi
